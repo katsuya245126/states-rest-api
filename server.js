12e70eabf4309c32335bcd96685de1c653d7c79c
John <katsuya245126@gmail.com>
2024-04-26 20:33:01 +0900

    Add dbConfig method and connect to server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const connectDB = require("./config/dbConn");
 const PORT = process.env.PORT || 3500;
