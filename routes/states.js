a7f0018b20b5152d8e49ef40041b0cbb7433c5fa
John <katsuya245126@gmail.com>
2024-04-28 22:11:06 +0900

    Add catch all at end of route and make 404 page

--- routes/states.js
+++ routes/states.js
@@ -2,12 +2,15 @@ const express = require("express");
 const router = express.Router();
 const validateState = require("../middleware/validateState");
 const statesController = require("../controllers/statesController");
+const path = require("path");
+
+// GET METHODS
 
 router.get("/", (req, res) => {
-  if (req.query.contig) {
-    return statesController.getStatesByContiguity(req, res);
-  }
-  statesController.getAllStates(req, res);
+    if (req.query.contig) {
+        return statesController.getStatesByContiguity(req, res);
+    }
+    statesController.getAllStates(req, res);
 });
 
 router.get("/:state", validateState, statesController.getStateData);
@@ -15,23 +18,31 @@ router.get("/:state", validateState, statesController.getStateData);
 router.get("/:state/capital", validateState, statesController.getStateCapital);
 
 router.get(
-  "/:state/nickname",
-  validateState,
-  statesController.getStateNickname
+    "/:state/nickname",
+    validateState,
+    statesController.getStateNickname
 );
 
 router.get(
-  "/:state/population",
-  validateState,
-  statesController.getStatePopulation
+    "/:state/population",
+    validateState,
+    statesController.getStatePopulation
 );
 
 router.get(
-  "/:state/admission",
-  validateState,
-  statesController.getStateAdmissionDate
+    "/:state/admission",
+    validateState,
+    statesController.getStateAdmissionDate
 );
 
 router.get("/:state/funfact", validateState, statesController.getRandomFunFact);
 
+router.all("*", (req, res) => {
+    // Check the Accept header or other indicators to decide between HTML and JSON
+    if (req.accepts("html")) {
+        res.status(404).sendFile(path.join(__dirname, "../public", "404.html"));
+    } else {
+        res.status(404).json({ error: "404 Not Found" });
+    }
+});
 module.exports = router;
