39937eb7195bbbdf81b7f1e07aa602d3980d32df
John <katsuya245126@gmail.com>
2024-04-28 22:54:22 +0900

    Moved express.json() middleware to before route definition

--- server.js
+++ server.js
@@ -7,9 +7,12 @@ const PORT = process.env.PORT || 3500;
 
 connectDB();
 
+// Middleware for json
+app.use(express.json());
+
 app.use("/states", require("./routes/states"));
 
 mongoose.connection.once("open", () => {
-  console.log("Connected to MongoDB");
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
